0f21020249bb0ddce17eb31d49cd41cfcde045be
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-09-23 13:12:03 -0400

    Update slime.gd

--- scenes/slime.gd
+++ scenes/slime.gd
@@ -4,3 +4,5 @@ extends CharacterBody2D
 
 func _process(delta: float) -> void:
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
+	if get_parent().get_progress_ratio() == 1:
+		queue_free()
