69fce86c29eea0a524f43497cb949c77960b2eee
Jackson Featherston <jacksonleeolaf@gmail.com>
2024-09-27 12:41:27 -0400

    Update red_bullet.gd

--- red_bullet.gd
+++ red_bullet.gd
@@ -1,11 +1,27 @@
 extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var target
+var Speed = 1000
+var pathName = ""
+var bulletDamage
+
+func _physics_process(delta):
+	var pathSpawnerNode = get_tree().get_root().get_node("Main/PathSpawner")
+	for i in pathSpawnerNode.get_child_count():
+		if pathSpawnerNode.get_child(i).name == pathName:
+			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
+
+	velocity = global_position.direction_to(target) +Speed
+
+	look_at(target)
+
+	move_and_slide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+func _on_area_2d_body_entered(body):
+	if "Slime A" in body.name:
+		body.Health -= bulletDamage
+		queue_free()
